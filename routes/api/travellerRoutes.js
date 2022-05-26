899fc028ef58b2dfed549d631d440bf68b4cdfcd
Leo Wong <leoelicos@gmail.com>
2022-05-26 14:09:32 +1000

    Join Trip to Traveller

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { Traveller } = require('../../models');
+const { Traveller, Trip } = require('../../models');
 
 // GET all travellers
 router.get('/', async (req, res) => {
 	try {
-		const travellerData = await Traveller.findAll();
+		const travellerData = await Traveller.findAll({
+			include: [{ model: Trip }]
+		});
 		res.status(200).json(travellerData);
 	} catch (err) {
 		res.status(500).json(err);
@@ -14,7 +16,9 @@ router.get('/', async (req, res) => {
 // GET a single traveller
 router.get('/:id', async (req, res) => {
 	try {
-		const travellerData = await Traveller.findByPk(req.params.id);
+		const travellerData = await Traveller.findByPk(req.params.id, {
+			include: [{ model: Trips }]
+		});
 
 		if (!travellerData) {
 			res.status(404).json({ message: 'No traveller found with that id!' });
@@ -42,8 +46,8 @@ router.delete('/:id', async (req, res) => {
 	try {
 		const travellerData = await Traveller.destroy({
 			where: {
-				id: req.params.id,
-			},
+				id: req.params.id
+			}
 		});
 
 		if (!travellerData) {
