01405e22db4d762f0d736b8d3ddf2adcf66a4537
Leo Wong <leoelicos@gmail.com>
2022-05-26 12:19:00 +1000

    Implement Heroku render of DB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
+app.use(express.static('public'));
+app.use('/trips', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/assets/pages/trips.html'));
+});
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log('Now listening'));
 });
